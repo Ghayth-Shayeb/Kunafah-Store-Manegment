4c5a02fde812f825b319ab6ce05f19fb57c5d173
Ghayth Shayeb <ghaythshayeb@gmail.com>
2026-07-21 14:01:43 +0300

    replacing the inputs 1, 2, 3 by another method

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,8 +1,8 @@
 function dating() {
     const monthSelect = document.getElementById("input1");
     const currentMonth = new Date().getMonth() + 1;
-    const monthNames = ["يناير","قبراير","مارس","إبريل","مايو","يونيو","يوليو","أغسطس","سبتمبر","أكتوبر","نوفمبر","ديسمبر"];
-    monthSelect.innerHTML = "";
+    const monthNames = ["يناير","فبراير","مارس","إبريل","مايو","يونيو","يوليو","أغسطس","سبتمبر","أكتوبر","نوفمبر","ديسمبر"];
+    monthSelect.innerHTML = '<option selected value="">اختر الشهر.</option>';
 
     for (let m = currentMonth; m <= 12; m++) {
         const option = document.createElement("option");
@@ -11,4 +11,20 @@ function dating() {
         monthSelect.appendChild(option);
     }
 }
-window.addEventListener("DOMContentLoaded", dating);
+
+function day(){
+    const daySelect = document.getElementById("input2");
+    const currentDay = new Date().getDate();
+    daySelect.innerHTML = '<option selected value="">اختر تاريخ معين, يوم 20 من الشهر كمثال</option>';
+
+    for (let d = currentDay; d <= 31; d++){
+        const option = document.createElement("option");
+        option.value = d;
+        option.textContent = d;
+        daySelect.appendChild(option);
+    }
+}
+window.addEventListener("DOMContentLoaded", () => {
+    dating();
+    day();
+});
