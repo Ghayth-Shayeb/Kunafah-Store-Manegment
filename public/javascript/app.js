5c640d084914424ea629fe4c741ddccb9793cfc0
Ghayth Shayeb <ghaythshayeb@gmail.com>
2026-08-02 19:44:45 +0300

    adding a new method for date

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,13 +15,47 @@ function dating() {
 function day(){
     const daySelect = document.getElementById("input2");
     const currentDay = new Date().getDate();
+    const currentMonth = new Date().getMonth() + 1;
     daySelect.innerHTML = '<option selected value="">اختر تاريخ معين, يوم 20 من الشهر كمثال</option>';
-
-    for (let d = currentDay; d <= 31; d++){
-        const option = document.createElement("option");
-        option.value = d;
-        option.textContent = d;
-        daySelect.appendChild(option);
+    if (currentMonth === new Date().getMonth() + 1) {
+        for (let d = currentDay; d <= 31; d++) {
+            const option = document.createElement("option");
+            option.value = d;
+            option.textContent = d;
+            daySelect.appendChild(option);
+        }
+    }
+    else if (monthSelect.value === monthNames[1]) {
+        for (let d = 1; d <= 28; d++) {
+            const option = document.createElement("option");
+            option.value = d;
+            option.textContent = d;
+            daySelect.appendChild(option);
+        }
+    }
+    else if (monthSelect.value === monthNames[3] || monthSelect.value === monthNames[5] || monthSelect.value === monthNames[8] || monthSelect.value === monthNames[10]) {
+        for (let d = 1; d <= 30; d++) {
+            const option = document.createElement("option");
+            option.value = d;
+            option.textContent = d;
+            daySelect.appendChild(option);
+        }
+    }
+    else if (monthSelect.value === monthNames[0] || monthSelect.value === monthNames[2] || monthSelect.value === monthNames[4] || monthSelect.value === monthNames[6] || monthSelect.value === monthNames[7] || monthSelect.value === monthNames[9] || monthSelect.value === monthNames[11]) {
+        for (let d = 1; d <= 31; d++) {
+            const option = document.createElement("option");
+            option.value = d;
+            option.textContent = d;
+            daySelect.appendChild(option);
+        }
+    }
+    else{
+        for (let d = 1; d <= 31; d++) {
+            const option = document.createElement("option");
+            option.value = d;
+            option.textContent = d;
+            daySelect.appendChild(option);
+        }
     }
 }
 window.addEventListener("DOMContentLoaded", () => {
